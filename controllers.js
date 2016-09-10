d2572d3979aa947c2f9ec2fc77497aa023695dbc
YOUR FULL NAME <morris.jess26@gmail.com>
2016-09-10 16:03:50 -0600

    design for the front end

---
+++ controllers.js
@@ -0,0 +1,16 @@
+// 'use strict';
+//
+// var app = angular.module('lavish', []);
+//
+// app.controller('myMap', function($scope) {
+//   $scope.imgurl = constructImgUrl();
+// });
+//
+// function constructImgUrl(){
+//   var mapDiv = document.getElementById('map');
+//   console.log('you are here');
+//   var mapImage = document.createElement('img');
+//   mapImage.src = encodeURI('https://maps.googleapis.com/maps/api/staticmap?center=48.188355,-117.029522&size=400x400&zoom=12&maptype=hybrid&key=AIzaSyD9fzZdcJQqdmt2NIsfJ1XnKTvNfc9SbmI');
+//   mapImage.alt = 'Lavish Salon Map';
+//   mapDiv.appendChild(mapImage);
+// }
